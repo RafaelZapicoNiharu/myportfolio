8413be00e6d96c68707bda25f89dcc99cbbd7bff
rafaelZapico <rafaelzapico00@gmail.com>
2023-10-16 11:02:08 -0300

    continuando de onde eu parei

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById('menu-config');
+
+function toggleMenu() {
+    
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
