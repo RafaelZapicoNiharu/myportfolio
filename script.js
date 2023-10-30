3b024597ce719d1eade108c6147048ee302c2442
rafaelZapico <rafaelzapico00@gmail.com>
2023-10-30 12:38:39 -0300

    separando os styles em dois e melhorando menu

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+// menuzinho de config abrir quando clica no icone 
 const menu = document.getElementById('menu-config');
+const menuIcone = document.querySelector('.fa-gear')
 
-function toggleMenu() {
+function toggleMenu(event) { // abre e fecha o menu
     
     if (menu.style.display === 'none' || menu.style.display === '') {
         menu.style.display = 'block';
     } else {
         menu.style.display = 'none';
     }
-}
+ 
+}  
+
+const pageContent = document.documentElement;  //aqui ta fechando se clica fora
+pageContent.addEventListener('click', function (event) {
+    if (event.target !== menu && event.target !== menuIcone) {
+        menu.style.display = 'none';
+    }
+});
+
