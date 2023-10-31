040caa62d44f7a50b27d7311aeb6b4ab72b32724
Niharu <rafaelzapico.ti@gmail.com>
2023-10-31 16:56:48 -0300

    adicionando swiper

--- script.js
+++ script.js
@@ -21,4 +21,27 @@ pageContent.addEventListener('click', function (event) {
 
 
 
+//swiper 
+
+const swiper = new Swiper('.swiper', {
+
+    direction: 'horizontal',
+    loop: true,
+  
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:'true'
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+ 
+  });
+
+
+
 
