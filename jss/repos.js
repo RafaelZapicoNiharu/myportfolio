77461b65c3c0ca5663f0f97f18e2faeb5bec9de1
NiharuDev <rafaelzapico.ti@gmail.com>
2023-12-18 12:08:36 -0300

    tirando coisas inuteis

--- jss/repos.js
+++ jss/repos.js
@@ -6,10 +6,10 @@ function getMeusProjetos() {
     fetch(`https://api.github.com/users/${meuUser}/repos?sort=updated`)
         .then(async response => {
             const dados = await response.json()
-            const projetosRecentes = dados.slice(0, 6);
-            console.log(projetosRecentes);
+            const projetosRecentes = dados.slice(0, 6); //pega os 6 primeiros
+            // console.log(projetosRecentes);
 
-            projetosRecentes.map(projeto => {
+            projetosRecentes.map(projeto => {   //pra cada objeto eu crio um card novo com suas informações
                 const novoProj = document.createElement('div');
                 novoProj.innerHTML =
                     `<div class="itemProj">
@@ -20,24 +20,14 @@ function getMeusProjetos() {
                 <div class="atualizadoProj">
                 ${ Intl.DateTimeFormat('pt-BR').format(new Date) }
                 </div>
+                <div class="linguagemProj">
+                ${projeto.language }
+                </div>
                 </a>
                 </div>`
-
-
-
-
-
-
                 projetos.appendChild(novoProj);
             })
-
-
-
-
-
         })
-        .catch(error => console.log(error("Erro ao requisitar projetos.")))
-
+        .catch(error => console.log(error("Erro ao requisitar projetos.")))  
 }
-
 getMeusProjetos();
--- jss/script.js
+++ jss/script.js
@@ -1,15 +1,13 @@
-// menuzinho de config abrir quando clica no icone 
+// menu de config abrir quando clica no icone 
 const menu = document.getElementById('menu-config');
 const menuIcone = document.querySelector('.fa-gear')
 
 function toggleMenu() { // abre e fecha o menu
-    
     if (menu.style.display === 'none' || menu.style.display === '') {
         menu.style.display = 'block';
     } else {
         menu.style.display = 'none';
     }
- 
 }  
 
 const pageContent = document.documentElement;  //aqui ta fechando se clica fora
@@ -24,57 +22,37 @@ pageContent.addEventListener('click', function (event) {
 //swiper 
 
 const swiper = new Swiper('.swiper', {
-
     direction: 'horizontal',
     loop: true,
-  
-
     pagination: {
       el: '.swiper-pagination',
       clickable:'true'
     },
-
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
- 
   });
 
 
-//alertinhas
 
+//alertas
 document.addEventListener('DOMContentLoaded', function () {
     var alertDiv = document.getElementById('alerta-do-dev');
     var alertDiv1 = document.getElementById('alerta-do-dev1');
-    
     var closeButton = document.querySelector('.close-button');
     var closeButton1 = document.querySelector('.dois');
     console.log(closeButton1);
-
     closeButton.addEventListener('click', function () {
         alertDiv.style.display = 'none'; 
     });
-
     closeButton1.addEventListener('click', function () {
         alertDiv1.style.display = 'none';
-     
-    });
-
-
+    })
     setTimeout(function () {
         alertDiv.style.display = 'none';
-    }, 6000); 
-
+    }, 6000);
     setTimeout(function () {
         alertDiv1.style.display = 'none';
     }, 10000); 
-
 });
-
-//
-
-
-
-
